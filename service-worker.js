0085088d7127738c795d40e6f4c4435392da6eb2
jcbouchaud <luciecamilleory@gmail.com>
2019-12-13 16:21:02 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/profiler/precache-manifest.d1e7a56ac76833114c9b792c78112dfb.js"
+  "/profiler/precache-manifest.28ec989cf1b52e51de310ed0bdaf7b56.js"
 );
 
 self.addEventListener('message', (event) => {
